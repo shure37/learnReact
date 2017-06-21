26e1ac517c40f0e534145ce4c17b3a48224f449f
楚奇 <chuqiw030@gmail.com>
2017-06-21 23:07:59 +0800

    routing

--- 2-react-router/src/js/pages/Layout.js
+++ 2-react-router/src/js/pages/Layout.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { Link } from "react-router";
 
+import Footer from "../components/layout/Footer";
+import Nav from "../components/layout/Nav";
+
 export default class Layout extends React.Component {
-  navigate(){
-    console.log(this.props);
-    this.props.history.pushState(null, "/");
-  }
   render(){
+    const {location} = this.props;
+    const containerStyle = {
+      marginTop: "60px"
+    };
+
     return (
       <div>
-        <h1>KillerNews.net</h1>
-        {this.props.children}
-        <Link to="archives">archives</Link>
-        <Link to="settings">settings</Link>
-        <button onClick={this.navigate.bind(this)}>featured</button>
+        <Nav location={location}/>
+
+        <div class="container" style={containerStyle}>
+          <div class="row">
+            <div class="col-lg-12">
+              <h1>KillerNews.net</h1>
+              {this.props.children}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
