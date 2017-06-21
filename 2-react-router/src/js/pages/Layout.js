272fcb34753e0b7c273deccb3523c89b49b33a88
楚奇 <chuqiw030@gmail.com>
2017-06-21 13:46:00 +0800

    no message

--- 2-react-router/src/js/pages/Layout.js
+++ 2-react-router/src/js/pages/Layout.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router";
 
 export default class Layout extends React.Component {
+  navigate(){
+    console.log(this.props);
+    this.props.history.pushState(null, "/");
+  }
   render(){
     return (
-      <h1>KillerNews.net</h1>
+      <div>
+        <h1>KillerNews.net</h1>
+        {this.props.children}
+        <Link to="archives">archives</Link>
+        <Link to="settings">settings</Link>
+        <button onClick={this.navigate.bind(this)}>featured</button>
+      </div>
     );
   }
 }
